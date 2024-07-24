768d9571f1f4167ca8d17f841bd7707bf07b1907
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-07-24 21:08:41 +0300

    Update Sound.cs

--- src/gameobject/components/audio/Sound.cs
+++ src/gameobject/components/audio/Sound.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Xna.Framework.Media;
+﻿using Microsoft.Xna.Framework.Audio;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,22 @@ namespace SerpentEngine {
         public string Path { get; set; } = "";
         public float Volume { get; set; } = 1f;
 
-        public Song SoundEffect { get; set; }
+        public bool isPlaying { get; set; } = false;
+
+        public SoundEffect SoundEffect { get; set; }
 
         public Sound(string path) : base(false)
         {
+            FileStream fileStream = new FileStream(path + ".wav", FileMode.Open);
+            SoundEffect = SoundEffect.FromStream(fileStream);
 
+        }
+
+
+        public virtual void Play()
+        {
+            SoundEffect.Play();
+            isPlaying = true;
         }
 
 
