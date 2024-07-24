80f82dd4c2a2f1ce3b49f47ad660f3baa8fd02a5
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-07-24 16:16:33 +0300

    Sound component title

---
+++ src/gameobject/components/audio/Sound.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerpentEngine { 
+    public class Sound : Component
+    {
+        public string Path { get; set; } = "";
+        public float Volume { get; set; } = 1f;
+
+        public Song SoundEffect { get; set; }
+
+        public Sound(string path) : base(false)
+        {
+
+        }
+
+
+
+    }
+}
