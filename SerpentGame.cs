2008310f1e8e0d4dd917a8577e144220cd42166b
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-07-24 10:26:14 +0300

    No more cheesed

--- SerpentGame.cs
+++ SerpentGame.cs
@@ -1,5 +1,4 @@
-﻿using Cheesed;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Diagnostics;
