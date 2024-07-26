edf47a07dc5dc400d1d60001443afab0156ade4e
Warhead <warhead51707xbox@gmail.com>
2024-07-26 02:19:39 -0400

    gameobject gui + gui setup

---
+++ src/debug/DebugGui.cs
@@ -0,0 +1,67 @@
+﻿using ImGuiNET;
+using SerpentEngine;
+
+namespace SerpentEngine;
+public class DebugGui : ImGuiDrawable
+{
+    private bool showGameObjectsWindow = false;
+
+    public override void Draw()
+    {
+       MainMenuBar();
+
+       if (showGameObjectsWindow)
+       {
+           GameObjectsWindow();
+       }
+    }
+
+    public void MainMenuBar()
+    {
+        if (ImGui.BeginMainMenuBar())
+        {
+            if (ImGui.BeginMenu("Scene"))
+            {
+                if (ImGui.MenuItem("GameObjects"))
+                {
+                    showGameObjectsWindow = true;
+                }
+            }
+        }
+    }
+
+    public void GameObjectsWindow()
+    {
+        ImGui.Begin("GameObjects", ref showGameObjectsWindow);
+
+        ImGui.Text("GameObjects: " + SceneManager.CurrentScene.GameObjects.Count);
+
+        ImGui.SeparatorText("GameObjects");
+
+        foreach (GameObject gameObject in SceneManager.CurrentScene.GameObjects)
+        {
+            if (ImGui.CollapsingHeader(gameObject.ToString()))
+            {
+                ImGui.SeparatorText("Properties");
+
+                ImGui.Text("Position: " + gameObject.Position);
+
+                ImGui.SeparatorText("Components");
+
+                foreach (Component component in gameObject.Components.Components)
+                {
+                    ImGui.Text(component.ToString());
+                }
+
+                ImGui.SeparatorText("Actions");
+
+                if (ImGui.Button("Remove"))
+                {
+                    SceneManager.CurrentScene.Remove(gameObject);
+
+                    break;
+                }
+            }
+        }
+    }
+}
