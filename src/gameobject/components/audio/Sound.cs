84f4395f6e71f156c0f27b13b49052e69e6565b4
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-07-30 22:13:30 +0300

    Welcome back sound component (R.I.P Sound not as component 2024 - 2024)

--- src/gameobject/components/audio/Sound.cs
+++ src/gameobject/components/audio/Sound.cs
@@ -2,13 +2,13 @@
 using System.IO;
 
 namespace SerpentEngine;  
-public class Sound
+public class Sound : Component
 {
     public string Path { get; private set; } = "";
     public float Volume { get; private set; } = 1f;
     public bool IsPlaying { get; private set; } = false;
     public SoundEffect SoundEffect { get; private set; }
-    public Sound(string path)
+    public Sound(string path) : base(false)
     {
         FileStream fileStream = new FileStream(path + ".wav", FileMode.Open);
         SoundEffect = SoundEffect.FromStream(fileStream);
