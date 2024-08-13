fe3addfc71d4be5369579193e233ca1908c5f2bd
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-08-13 20:41:34 +0300

    Update Collision.cs

--- src/gameobject/components/Collision.cs
+++ src/gameobject/components/Collision.cs
@@ -1,14 +1,35 @@
 ﻿using Microsoft.Xna.Framework;
 
-namespace SerpentEngine
-{
-    public delegate void CollisionEvent(GameObject target);
-    public class Collision : Component
-    {
-        public event CollisionEvent OnCollide;
-        public Rectangle Box { get; private set; } = Rectangle.Empty;
+namespace SerpentEngine;
 
-        public Collision(Vector2 position, Vector2 dimensions) : base(false)
+public delegate void CollisionEvent(GameObject target);
+public class Collision : Component
+{
+    public event CollisionEvent OnCollide;
+    public Rectangle Box { get; private set; } = Rectangle.Empty;
+
+    public Collision(Vector2 position, Vector2 dimensions) : base(false)
+    {
+        Box = new Rectangle((int)position.X, (int)position.Y, (int)dimensions.X, (int)dimensions.Y);
+    }
+
+    public static Collision Empty()
+    {
+        return new Collision(Vector2.Zero, Vector2.Zero);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        Box = new Rectangle((int)GameObject.Position.X, (int)GameObject.Position.Y, Box.Width, Box.Height);
+
+        CheckCollision();
+    }
+
+    private void CheckCollision()
+    {
+        foreach (GameObject target in SceneManager.CurrentScene.GameObjects)
         {
             Box = new Rectangle((int)position.X, (int)position.Y, (int)dimensions.X, (int)dimensions.Y);
         }
@@ -44,3 +65,4 @@ namespace SerpentEngine
         }
     }
 }
+    
