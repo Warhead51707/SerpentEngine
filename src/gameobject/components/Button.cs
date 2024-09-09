568292fa4554d2f6d9c1eb2ef023863c84695e11
Warhead <warhead51707xbox@gmail.com>
2024-09-09 01:25:52 -0400

    Update Button.cs

--- src/gameobject/components/Button.cs
+++ src/gameobject/components/Button.cs
@@ -8,19 +8,18 @@ using System.Threading.Tasks;
 
 namespace SerpentEngine;
 
-public delegate void ClickEvent(Vector2 position);
+public delegate void ClickEvent();
 public class Button : Component
 {
     public event ClickEvent OnClick;
     public Vector2 Size { get; set; } = Vector2.Zero;
     public Button(Vector2 size) : base(false)
     {
-        size = Size;
+        Size = size;
     }
 
     public override void Update()
     {
-
         CheckClick();
         base.Update();
     }
@@ -28,20 +27,15 @@ public class Button : Component
     public void CheckClick()
     {
         Rectangle box = new Rectangle((int)GameObject.Position.X, (int)GameObject.Position.Y, (int)Size.X, (int)Size.Y);
-        Vector2 screenPosition = Input.Mouse.GetNewPosition();
-        Vector2 screenCenter = new Vector2(GraphicsConfig.SCREEN_WIDTH / 2, GraphicsConfig.SCREEN_HEIGHT / 2);
-        Vector2 worldPosition = (screenPosition - screenCenter) / SceneManager.CurrentScene.Camera.Zoom + SceneManager.CurrentScene.Camera.Position;
+
+        Vector2 screenPosition = Input.Mouse.GetNewPosition() / SceneManager.CurrentScene.Camera.UIScale;
 
         if (!Input.Mouse.LeftClick()) return;
         
-        DebugGui.Log(new Vector2((int)GameObject.Position.X, (int)GameObject.Position.Y) + "     " + worldPosition);
-        if (box.Contains(worldPosition))
+        if (box.Contains(screenPosition))
         {
-            OnClick.Invoke(worldPosition);
+            OnClick.Invoke();
         }
 
     }
-
-
-
 }
