b17230892d8c126e27619ec79cb2cb2008fadfc6
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-10-22 18:43:54 +0300

    Soundplayer

--- src/gameobject/component/audio/Sound.cs
+++ src/gameobject/component/audio/Sound.cs
@@ -1,30 +1,26 @@
 ﻿using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace SerpentEngine;  
-public class Sound : Component
+namespace SerpentEngine;
+
+public class Sound
 {
-    public string Path { get; private set; } = "";
-    public float Volume { get; private set; } = 1f;
-    public bool IsPlaying { get; private set; } = false;
-    public SoundEffect SoundEffect { get; private set; }
-    public Sound(string path) : base(false)
-    {
-        FileStream fileStream = new FileStream(path + ".wav", FileMode.Open);
-        SoundEffect = SoundEffect.FromStream(fileStream);
 
-    }
+    public string Path { get; set; } = "";
 
-    public void ChangeVolume(float volume)
+    public float Volume { get; set; } = 0.5f;
+
+    public Sound(string path)
     {
-        Volume = volume;
-        SoundEffect.MasterVolume = Volume;
+        Path = path;
     }
 
 
-    public virtual void Play()
-    {
-        SoundEffect.Play();
-        IsPlaying = true;
-    }
+
+
 }
---
+++ src/gameobject/component/audio/SoundPlayer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using System.IO;
+
+namespace SerpentEngine;  
+public class SoundPlayer : Component
+{
+    public SoundPlayer() : base(false)
+    {
+
+    }
+
+    public virtual void PlaySound(Sound sound)
+    {
+        SoundEffect soundEffect;
+        FileStream fileStream = new FileStream(sound.Path + ".wav", FileMode.Open);
+        soundEffect = SoundEffect.FromStream(fileStream);
+        soundEffect.Play();
+
+    }
+}
